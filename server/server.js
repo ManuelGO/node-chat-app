aff84dc655f50a2698ad58e1d9b520a6c58ef7ff
Manuel Gonzalez <manuelarvelo19@gmail.com>
2017-06-09 20:02:38 -0400

    Add connect and disconnect hadler events

--- server/server.js
+++ server/server.js
@@ -1,17 +1,28 @@
 const path = require('path'); //path no require ser instalado con npm install.
+const http = require('http'); //no require ser instalado.
 const express = require('express');
+const socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server); //para comunicarnos cliente-server y viceversa.
 
 app.use(express.static(publicPath));
 
-app.get('/', (req, res)=>{
-	res.send('Hola mundo');
+
+io.on('connection', (socket)=>{
+	console.log('New user connected');
+
+	socket.on('disconnect', ()=>{
+		console.log('user gone');
+	})
 });
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
 	console.log(`Server running at port: ${port}`);
 });
 
